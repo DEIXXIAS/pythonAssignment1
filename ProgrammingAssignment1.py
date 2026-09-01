4fd47ed66b313e81c85ab8247f4e0f8b6645d815
Vincent M <159970260+DEIXXIAS@users.noreply.github.com>
2026-08-31 21:42:10 -0500

    newest update

--- ProgrammingAssignment1.py
+++ ProgrammingAssignment1.py
@@ -1,4 +1,30 @@
-# (1)
+# ------------------------------------------------- main functions ------------------------------------------------------------
+
+def deleteFirstRecord (table):  
+    return table[1:] # returns a new list without the first record
+ 
+
+
+
+def displayRecordsOnScreen (table):
+        for index in range(len(table)):
+            (stockID, price, stockDescr, itemsOnStock, itemsOrdered) = table(index)
+
+        print("%-2d%-8s%-8.2f%-29s%-4d%-2d" % (int(index + 1), stockID, float(price), stockDescr, int(itemsOnStock), int(itemsOrdered) ) )
+              
+
+
+
+
+
+
+
+
+
+
+
+# ------------------------------------------------ MAIN PROGRAM -----------------------------------------------------------------
+
 infile = open('invento.txt')
 
 # (2)
@@ -12,8 +38,21 @@ table = list() # starts with an empty list
 for data in infile:
     table.append( tuple(data.split() ))
 
-# (5)
-print(table) # displays the table of records
+option = '0'
+while option != 'Q':
+    print("========MENU========")
+    print("1. Delete the first record")
+    print("8. Display the records on the screen")
+    print("Q. Quit")
+
+    option = input("\nEnter your option: ")
+
+
+    if option == '9':
+        displayRecordsOnScreen(table)
+
+print("\n\n")
+print(table) # displays the table records
 
 #(6) # closes the files
 infile.close()
